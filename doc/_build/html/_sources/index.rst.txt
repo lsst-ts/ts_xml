d800f9cbceb489540025330bbb1667134bc851a4
Andrew Heyer <aheyer@lsst.org>
2019-03-18 11:30:37 -0700

    adding some template doc items

---
+++ doc/_build/html/_sources/index.rst.txt
@@ -0,0 +1,26 @@
+.. _astro_metadata_translator-package:
+
+.. Title is the EUPS package name
+
+#########################
+ts_xml
+#########################
+
+.. Sentence/short paragraph describing what the package is for.
+
+The ``ts_xml`` is package is used for defining the data objects for all the Commandable SAL Components (CSC). These data objects are defined in XML. SAL consumes the XML to produce language specific libraries to be able to communicate over the network. 
+
+The following is a table overview of all the CSC's defined so far.
+
+
+
+
+Project info
+============
+
+Repository
+   https://github.com/lsst-ts/ts_xml
+
+
+Python API reference
+====================
---
+++ doc/conf.py
@@ -0,0 +1,28 @@
+"""Sphinx configuration file for an LSST stack package.
+
+This configuration only affects single-package Sphinx documenation builds.
+"""
+
+from documenteer.sphinxconfig.stackconf import build_package_configs
+# import ts_xml
+# import ts_xml.version
+
+
+globals().update(build_package_configs(
+    project_name="ts_xml",
+    version="develop"))
+
+
+# Remove ts_xml from the default intersphinx configuration
+try:
+    del intersphinx_mapping['ts_xml']  # noqa
+except KeyError:
+    pass
+
+
+# Add pipelines.lsst.io to the intersphinx configuration.
+# NOTE: we might want to be more sophisticated about mapping corresponding
+# versions of the Pipelines and ts_xml. This technique will
+# mostly work if the Pipelines and ts_xml are developed
+# concurrently.
+intersphinx_mapping['lsst'] = ('https://pipelines.lsst.io/v/daily/', None)  # noqa
