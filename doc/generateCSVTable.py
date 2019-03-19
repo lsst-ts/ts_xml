51b8ae946babb59b660d79c968c3c5dccd7f849a
Andrew Heyer <aheyer@lsst.org>
2019-03-19 09:57:01 -0700

    adding tables

--- doc/generateCSVTable.py
+++ doc/generateCSVTable.py
@@ -18,45 +18,48 @@ for member in root.findall('Subsystem'):
 	subsystem = []
 
 
+	# Here we add the table header. It may be redundent to fine the names of the
+	# XML tag however I do it anyways to be sure we arent adding a header for an
+	# XML tag that is non-existent. 
 	if count == 0:
 		name = member.find('Name').tag
-		subsystem_head.append(name)
+		subsystem_head.append('Subystem')
 		
 		active_developers = member.find('ActiveDevelopers').tag
-		subsystem_head.append(active_developers)
+		subsystem_head.append("Active Developers")
 
 		CAM = member.find('CAM').tag
 		subsystem_head.append(CAM)
 
 		github = member.find('Github').tag
-		subsystem_head.append(github)
+		subsystem_head.append('Github')
 
 		jenkins_test_results = member.find('JenkinsTestResults').tag
-		subsystem_head.append(jenkins_test_results)
+		subsystem_head.append("Jenkins Test Results")
 
 		LSSTPoC = member.find('LSSTPoC').tag
-		subsystem_head.append(LSSTPoC)
+		subsystem_head.append('LSST PoC')
 
 		LSSTIO = member.find('LSSTIO').tag
-		subsystem_head.append(LSSTIO)
+		subsystem_head.append('LSST.io')
 
 		product_owner = member.find('ProductOwner').tag
-		subsystem_head.append(product_owner)
+		subsystem_head.append('Product Owner')
 
 		related_documents = member.find('RelatedDocuments').tag
-		subsystem_head.append(related_documents)
+		subsystem_head.append('Related Documents')
 
 		software_language = member.find('SoftwareLanguage').tag
-		subsystem_head.append(software_language)
+		subsystem_head.append('Software Language')
 
 		vendor_PoC = member.find('VendorPoC').tag
-		subsystem_head.append(vendor_PoC)
+		subsystem_head.append('Vendor PoC')
 
 		XML = member.find('XML').tag
-		subsystem_head.append(XML)
+		subsystem_head.append('XML')
 
 		principal_system = member.find('PrincipalSystem').tag
-		subsystem_head.append(principal_system)
+		subsystem_head.append('Principal System')
 
 		csvwriter.writerow(subsystem_head)
 		count = count + 1
