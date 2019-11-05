b15aa28c5828e4f56f10dd6f0ea3705c8bfe02b8
rbovill <rbovill@lsst.org>
2019-11-05 14:14:00 -0700

    Finished the script to create the SALGenerics.xml python unit tests.  Created the unit test suite, test_Generics.py.

--- scripts/unittests/Generics.py
+++ scripts/unittests/Generics.py
@@ -3,10 +3,13 @@
 import xml.etree.ElementTree as ET
 import os
 import os.path
+import glob
 import xml_common
 
 # Create/Open test suite file.
-file = open("../../tests/test_Generics.py","w+")
+cwd = os.getcwd()
+file = open(cwd + "/../../tests/test_Generics.py","w+")
+sal_generics_file = "SALGenerics.xml"
 
 # Create Settings header.
 file.write("#!/usr/bin/env python\n")
@@ -21,115 +24,64 @@ file.write("import xml_common\n")
 file.write("\n")
 
 # Create Variables table.
-file.write("class TestGenerics(unittest.TestCase):\n")
-file.write("command_xml_path = '/SALObjects/SALCommandSet/SALCommand'")
-file.write("event_xml_path = '/SALObjects/SALEventSet/SALEvent'")
+file.write("class TestGenerics(unittest.TestCase):\n\n")
+
+# Create the Variables table.
+file.write("\t# Variables #\n")
+file.write("\ttree = ET.parse(cwd + \"/../sal_interfaces/" + sal_generics_file + "\")\n")
+file.write("\troot = tree.getroot()\n")
+file.write("\tgeneric_commands = xml_common.generic_commands\n")
+file.write("\tgeneric_events = xml_common.generic_events\n")
+file.write("\tcommand_xml_path = '/SALObjects/SALCommandSet/SALCommand'\n")
+file.write("\tevent_xml_path = '/SALObjects/SALEventSet/SALEvent'\n")
+file.write("\tcommands = []\n")
+file.write("\tevents = []\n")
 file.write("\n")
-
-# Get the Topics for each message type.
-try:
-	topics =  + command_xml_path + '/EFDB_Topic" -v . -n ' + home + '/sal_interfaces/' + sal_generics_file, shell=True).split()
-except:
-	print("\tERROR: " + sal_generics_file + + " for Commands is not valid XML.")
-
-# Get the Topics for each message type.
-try:
-	topics = subprocess.check_output(app + ' sel -t -m "/' + event_xml_path + '/EFDB_Topic" -v . -n ' + home + '/sal_interfaces/' + sal_generics_file, shell=True).split()
-except:
-	print("\tERROR: " + sal_generics_file + " for Events is not valid XML.")
-
-skipped=""
-com_skipped=""
-event_skipped=""
-enum_skipped=""
 
 # Create the Test Cases.
-# Validate the sal_generics_file.
-file.write("Validate " + sal_generics_file + "\n")
-file.write("\t[Documentation]    Validate the " + sal_generics_file + " dictionary file.\n")
-file.write("\t[Tags]    smoke\n")
-file.write("\t${output}=    Run    ${xml} val -e ${folder}/sal_interfaces/" + sal_generics_file + "\n")
-file.write("\tLog    ${output}\n")
-file.write("\tShould Contain    ${output}   " + sal_generics_file + " - valid\n")
-file.write("\n")
-
 # Validate the sal_generics_file defines the Generic Commands.
-file.write("Validate " + sal_generics_file + " Generic Commands\n")
-file.write("\t[Documentation]    Validate the " + sal_generics_file + " contains all the required generic, or State Machine, commands.\n")
-file.write("\t[Tags]    smoke    " + com_skipped + skipped + "\n")
-file.write("\tComment    Define CSC.\n")
-file.write("\tComment    Get the Commands for the CSC.\n")
-file.write("\t${topics}=    Run    ${xml} sel -t -m \"/" + command_xml_path + "/EFDB_Topic\" -v . -n ${folder}/sal_interfaces/" + sal_generics_file + "\n")
-file.write("\t@{Commands}=    Split to Lines    ${topics}\n")
-file.write("\t:FOR    ${command}    IN    @{GenericCommands}\n")
-file.write("\t\    ${string}=    Catenate   SEPARATOR=    SALGeneric_command_     ${command}\n")
-file.write("\t\    Run Keyword And Continue On Failure    Should Contain    ${Commands}    ${string}\n")
+file.write("\tdef test_" + os.path.splitext(sal_generics_file)[0] + "Commands(self):\n")
+file.write("\t\tfor alias in self.root.findall('./SALCommandSet/SALCommand/Alias'):\n")
+file.write("\t\t\tself.commands.append(alias.text)\n")
+file.write("\t\tself.commands.sort()\n")
+file.write("\t\tself.generic_commands.sort()\n")
+file.write("\t\tself.assertEqual(self.commands, self.generic_commands)\n")
 file.write("\n")
 
 # Validate the sal_generics_file defines the Generic Events.
-file.write("Validate " + sal_generics_file + " Generic Events\n")
-file.write("\t[Documentation]    Validate the " + sal_generics_file + " contains all the required generic events.\n")
-file.write("\t[Tags]    smoke    " + event_skipped + skipped + "\n")
-file.write("\tComment    Define CSC.\n")
-file.write("\tComment    Get the Events.\n")
-file.write("\t${topics}=    Run    ${xml} sel -t -m \"/" + event_xml_path + "/EFDB_Topic\" -v . -n ${folder}/sal_interfaces/" + sal_generics_file + "\n")
-file.write("\t@{Events}=    Split to Lines    ${topics}\n")
-file.write("\t:FOR    ${item}    IN    @{GenericEvents}\n")
-file.write("\t\    ${string}=    Catenate   SEPARATOR=    SALGeneric_logevent_    ${item}\n")
-file.write("\t\    Run Keyword And Continue On Failure    Should Contain    ${Events}    ${string}\n")
+file.write("\tdef test_" + os.path.splitext(sal_generics_file)[0] + "Events(self):\n")
+file.write("\t\tfor alias in self.root.findall('./SALEventSet/SALEvent/Alias'):\n")
+file.write("\t\t\tself.events.append(alias.text)\n")
+file.write("\t\tself.events.sort()\n")
+file.write("\t\tself.generic_events.sort()\n")
+file.write("\t\tself.assertEqual(self.events, self.generic_events)\n")
 file.write("\n")
 
 # Verify the CSCs DO NOT explicitly define the Generic Events.
-for subsystem in xml_common.subsystems:
-	events_file = glob.glob(os.environ['XML_HOME'] + "/sal_interfaces/" + subsystem + "/" + subsystem + "_Events.xml")
+for csc in xml_common.subsystems:
+	events_file = glob.glob(cwd + "/../../sal_interfaces/" + csc + "/" + csc + "_Events.xml")
 	if events_file:
-		file.write("Validate " + subsystem + "_Events.xml Does Not Contain Generic Events\n")
-		file.write("\t[Documentation]    Validate the " + subsystem + "_Events.xml does not contain Generic Events.\n")
-		file.write("\t[Tags]    smoke    " + event_skipped + skipped + "\n")
-		file.write("\tComment    Get the CSC Events.\n")
-		file.write("\t${events}=    Run    ${xml} sel -t -m \"//SALEventSet/SALEvent/EFDB_Topic\" -v . -n ${folder}/sal_interfaces/" + subsystem + "/" + subsystem + "_Events.xml\n")
-		file.write("\tLog    ${events}\n")
-		file.write("\t:FOR    ${item}    IN    @{GenericEvents}\n")
-		file.write("\t\    Log Many    ${events}    " + subsystem + "_logevent_${item}\n")
-		file.write("\t\    Run Keyword And Continue On Failure    Should Not Contain    ${events}    " + subsystem + "_logevent_${item}\n")
+		csc_tree = ET.parse(cwd + "/../../sal_interfaces/" + csc + "/" + csc + "_Events.xml")
+		file.write("\tdef test_Validate" + csc + "DoesNotContainGenericEvents(self):\n")
+		file.write("\t\tself.csc_events = []\n")
+		file.write("\t\tself.csc_tree = ET.parse(cwd + \"/../sal_interfaces/" + csc + "/" + csc + "_Events.xml\")\n")
+		file.write("\t\tfor alias in self.csc_tree.findall('./SALEvent/Alias'):\n")
+		file.write("\t\t\tself.csc_events.append(alias.text)\n")
+		file.write("\t\tfor event in self.csc_events:\n")
+		file.write("\t\t\tself.assertTrue(event not in self.generic_events, msg=\"Generic event \" + event + \" is incorrectly defined in the " + csc + " Events XML\")\n")
 		file.write("\n")
 
-# Verify the CSCs DO NOT explicitly define the Generic Commands.
-	commands_file = glob.glob(os.environ['XML_HOME'] + "/sal_interfaces/" + subsystem + "/" + subsystem + "_Commands.xml")
+	commands_file = glob.glob(cwd + "/../../sal_interfaces/" + csc + "/" + csc + "_Commands.xml")
 	if commands_file:
-		file.write("Validate " + subsystem + "_Commands.xml Does Not Contain Generic Commands\n")
-		file.write("\t[Documentation]    Validate the " + subsystem + "_Commands.xml does not contain Generic Commands.\n")
-		file.write("\t[Tags]    smoke    " + com_skipped + skipped + "\n")
-		file.write("\tComment    Get the CSC Commands.\n")
-		file.write("\t${commands}=    Run    ${xml} sel -t -m \"//SALCommandSet/SALCommand/EFDB_Topic\" -v . -n ${folder}/sal_interfaces/" + subsystem + "/" + subsystem + "_Commands.xml\n")
-		file.write("\t:FOR    ${generic}    IN    @{GenericCommands}\n")
-		file.write("\t\    Run Keyword And Continue On Failure    Test Commands    ${commands}    " + subsystem + "_command_${generic}\n")
+		csc_tree = ET.parse(cwd + "/../../sal_interfaces/" + csc + "/" + csc + "_Commands.xml")
+		file.write("\tdef test_Validate" + csc + "DoesNotContainGenericCommands(self):\n")
+		file.write("\t\tself.csc_commands = []\n")
+		file.write("\t\tself.csc_tree = ET.parse(cwd + \"/../sal_interfaces/" + csc + "/" + csc + "_Commands.xml\")\n")
+		file.write("\t\tfor alias in self.csc_tree.findall('./SALEvent/Alias'):\n")
+		file.write("\t\t\tself.csc_commands.append(alias.text)\n")
+		file.write("\t\tfor event in self.csc_commands:\n")
+		file.write("\t\t\tself.assertTrue(event not in self.generic_commands, msg=\"Generic command \" + command + \" is incorrectly defined in the " + csc + " Commands XML\")\n")
 		file.write("\n")
 
-# Create custom keywords
-# Create Generic Commands Array
-file.write("*** Keywords ***\n")
-file.write("Create the Generic Commands Array\n")
-file.write("\t[Tags]    smoke\n")
-file.write("\t@{GenericCommands}=    Create List    start    enable    disable    standby\n")
-file.write("\tLog Many    @{GenericCommands}\n")
-file.write("\tSet Suite Variable    @{GenericCommands}\n")
-file.write("\n")
-
-# Create Generic Events Array
-file.write("Create the Generic Events Array\n")
-file.write("\t[Tags]    smoke\n")
-file.write("\t@{GenericEvents}=    Create List    appliedSettingsMatchStart    errorCode    settingVersions    summaryState\n")
-file.write("\tLog Many    @{GenericEvents}\n")
-file.write("\tSet Suite Variable    @{GenericEvents}\n")
-file.write("\n")
-
-# Create Command Validation keyword
-file.write("Test Commands\n")
-file.write("\t[Arguments]    ${commands}    ${generic}\n")
-file.write("\tComment    Convert to list.\n")
-file.write("\t@{commands}=    Split String    ${commands}\n")
-file.write("\t:FOR    ${item}    IN    @{commands}\n")
-file.write("\t\    Log Many    ${item}    ${generic}\n")
-file.write("\t\    Should Not Match    ${item}    ${generic}\n")
-file.write("\n")
+file.write("if __name__ == \"__main__\":\n")
+file.write("\tunittest.main()\n")
