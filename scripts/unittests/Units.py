1f36a6acfd2304dd2382fbf761a95aa52b8f5e22
rbovill <rbovill@lsst.org>
2019-11-13 16:18:09 -0700

    Parameterized the pathing to the XML files in the unit tests.

--- scripts/unittests/Units.py
+++ scripts/unittests/Units.py
@@ -15,6 +15,7 @@ file = open(cwd + "/../../tests/test_Units.py","w")
 # Create Settings header.
 file.write("#!/usr/bin/env python\n")
 file.write("# -*- coding: utf-8 -*-\n")
+file.write("import os\n")
 file.write("import unittest\n")
 file.write("import xml.etree.ElementTree as ET\n")
 file.write("import xml_common\n")
@@ -57,7 +58,9 @@ for csc in xml_common.subsystems:
 		## Verify the Units tag is not empty.
 		## Validate that the units conform to the astropy Units definitions.
 		file.write("\tdef test_" + csc + messageType + "UnitsValid(self):\n")
-		file.write("\t\tself.tree = ET.parse(\"" + xml[3:] + "\")\n")
+		file.write("\t\tself.dir_path = os.path.dirname(os.path.realpath(__file__))\n")
+		file.write("\t\tself.file = open(self.dir_path + '/" + xml[3:] + "')\n")
+		file.write("\t\tself.tree = ET.parse(self.file)\n")
 		file.write("\t\tself.root = self.tree.getroot()\n")
 		file.write("\t\tfor unit in self.root.findall('./SAL" + messageType.rstrip('s') + "/item/Units'):\n")
 		file.write("\t\t\tif not unit.text:\n")
@@ -66,6 +69,7 @@ for csc in xml_common.subsystems:
 		file.write("\t\t\t\tself.assertTrue(True)\n")
 		file.write("\t\t\telse:\n")
 		file.write("\t\t\t\tself.assertNotIn('Error', self.uv.check_unit(unit.text))\n")
+		file.write("\t\tself.file.close()\n")
 		file.write("\n")
 
 file.write("if __name__ == \"__main__\":\n")
