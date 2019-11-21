a02ad5b1c9c3f71564f2480bd510c131ee8a3b0d
rbovill <rbovill@lsst.org>
2019-11-21 14:34:30 -0700

    Converted test_Alias.py, test_AttributeDescription.py, test_AttributeNaming.py and test_Count.py to a pytest only implementation.

--- tests/test_Alias.py
+++ tests/test_Alias.py
@@ -1,42 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import glob
-import os
-import unittest
+import pathlib
+import pytest
 import xml.etree.ElementTree as ET
 import xml_common
 
-class TestAliases(unittest.TestCase):
+def get_arguments():
+	pkgroot = pathlib.Path(__file__).resolve().parents[1]
+	arguments = []
+	for csc in xml_common.subsystems:
+		xml_path = pkgroot / "sal_interfaces" / csc
+		for xml_file in xml_path.glob(f"{csc}_*.xml"):
+			message_type = xml_file.stem.split("_")[1]
+			if message_type == "Telemetry":
+				# Telemetry topics have no <Alias> tag.
+				continue
+			arguments.append((csc,xml_file,message_type))
+	return arguments
 
-	def setUp(self):
-		self.pkgroot = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-		self.sal_interfaces = os.path.join(self.pkgroot, "sal_interfaces")
+@pytest.mark.parametrize("csc,xml_file,message_type", get_arguments())
+def test_aliases(csc,message_type,xml_file):
+	"""Test that the <Alias> conforms to the <EFDB_Topic>.
+	
+	Telemetry files are excluded because telemetry topics have no aliases.
 
-	def test_aliases(self):
-		"""Test that the Alias name matches the EFDB_Topic name."""
-		for csc in xml_common.subsystems:
-			xml_paths = glob.glob(os.path.join(self.sal_interfaces, csc, f"{csc}_*.xml"))
-			for xml_path in xml_paths:
-				category = xml_path.split('/')[4].split('_')[1].split('.')[0]
-				if category == "Telemetry":
-					# Telemetry XML files have no Alias
-					continue
-				with self.subTest(xml_path=xml_path):
-					self.check_aliases(xml_path)    
-
-	def check_aliases(self, xml_path):
-		"""Check that the Alias name matches the EFDB_Topic name for all topics in a file."""
-		csc_aliases = []
-		csc_topics = []
-		with open(xml_path, "r", encoding="utf-8") as f:
-			tree = ET.parse(f)
-			root = tree.getroot()
-			csc_aliases = set(alias.text for alias in root.findall('./SALEvent/Alias'))
-			csc_topics = set(topic.text.split('_', 2)[2] for topic in root.findall('./SALEvent/EFDB_Topic'))
-			differences = csc_aliases ^ csc_topics
-			if len(differences) != 0:
-				self.fail(f"The aliases and topic names for {xml_path} are not an exact match: "
-							f"differences {differences}")
-
-if __name__ == "__main__":
-	unittest.main(verbosity=2)
+	Parameters
+	----------
+	csc : `xml_common.subsystems`
+		Name of the CSC
+	message_type : `xml_file.stem`
+		One of ['Commands','Events']
+	xml_file : `pathlib.Path`
+		Full filepath to the Commands or Events XML file for the CSC.	
+	"""
+	saltype = "SAL" + message_type.rstrip('s')
+	with open(str(xml_file), "r", encoding="utf-8") as f:
+		tree = ET.parse(f)
+		root = tree.getroot()
+		csc_aliases = set(alias.text for alias in root.findall(f"./{saltype}/Alias"))
+		csc_topics = set(topic.text.split('_', 2)[2] for topic in root.findall(f"./{saltype}/EFDB_Topic"))
+		assert csc_aliases == csc_topics
