1ac1cd6b990c8fbae895acac0965be714ca8eb05
rbovill <rbovill@lsst.org>
2019-11-22 11:13:47 -0700

    Updated test_Alias.py to use the xml_common.get_xmlfile_csc_topic() function, removed unncessary library imports and implemented a way to skip Telemetry tests.

--- tests/test_Alias.py
+++ tests/test_Alias.py
@@ -1,26 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import glob
-import pathlib
 import pytest
 import xml.etree.ElementTree as ET
 import xml_common
 
-def get_arguments():
-	pkgroot = pathlib.Path(__file__).resolve().parents[1]
-	arguments = []
-	for csc in xml_common.subsystems:
-		xml_path = pkgroot / "sal_interfaces" / csc
-		for xml_file in xml_path.glob(f"{csc}_*.xml"):
-			message_type = xml_file.stem.split("_")[1]
-			if message_type == "Telemetry":
-				# Telemetry topics have no <Alias> tag.
-				continue
-			arguments.append((csc,xml_file,message_type))
-	return arguments
-
-@pytest.mark.parametrize("csc,xml_file,message_type", get_arguments())
-def test_aliases(csc,message_type,xml_file):
+@pytest.mark.parametrize("xmlfile,csc,topic", xml_common.get_xmlfile_csc_topic())
+def test_aliases(xmlfile,csc,topic):
 	"""Test that the <Alias> conforms to the <EFDB_Topic>.
 	
 	Telemetry files are excluded because telemetry topics have no aliases.
@@ -34,10 +19,14 @@ def test_aliases(csc,message_type,xml_file):
 	xml_file : `pathlib.Path`
 		Full filepath to the Commands or Events XML file for the CSC.	
 	"""
-	saltype = "SAL" + message_type.rstrip('s')
-	with open(str(xml_file), "r", encoding="utf-8") as f:
-		tree = ET.parse(f)
+	if topic == "Telemetry":
+		pass
+	else:
+		saltype = "SAL" + topic.rstrip('s')
+		with open(str(xmlfile), "r", encoding="utf-8") as f:
+			tree = ET.parse(f)
 		root = tree.getroot()
 		csc_aliases = set(alias.text for alias in root.findall(f"./{saltype}/Alias"))
 		csc_topics = set(topic.text.split('_', 2)[2] for topic in root.findall(f"./{saltype}/EFDB_Topic"))
 		assert csc_aliases == csc_topics
+
