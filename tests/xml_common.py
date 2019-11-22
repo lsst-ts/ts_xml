dfdabc30fc31b310dd5b40526721986be3c2064d
rbovill <rbovill@lsst.org>
2019-11-22 10:58:07 -0700

    Renamed the mysql_reserved list to db_reserved, to be more generic. Removed obsolete functions and library imports.  Added the get_xmlfile_csc_topic, which is called by all test_*.py files.

--- tests/xml_common.py
+++ tests/xml_common.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import re
-import os
+import pathlib
 
 """This library defines common variables and functions used by the various XML test suite generator scripts.""" 
 
@@ -38,60 +37,50 @@ idl_reserved = [ 'ABSTRACT', 'ANY', 'ATTRIBUTE', 'BOOLEAN', 'CASE', 'CHAR', 'COM
 				'STRUCT', 'SUPPORTS', 'SWITCH', 'TRUE', 'TRUNCATABLE', 'TYPEDEF', 'TYPEID', 'TYPEPREFIX', \
 				'UNION', 'UNSIGNED', 'USES', 'VALUEBASE', 'VALUETYPE', 'VOID', 'WCHAR', 'WSTRING' ]
 
-mysql_reserved = [ 'ACCESSIBLE', 'ADD', 'ALL', 'ALTER', 'ANALYZE', 'AND', 'AS', 'ASC', 'ASENSITIVE', \
-					'BEFORE', 'BETWEEN', 'BIGINT', 'BINARY', 'BLOB', 'BOTH', 'BY', 'CALL', 'CASCADE', \
-					'CASE', 'CATALOG_NAME', 'CHANGE', 'CHAR', 'CHARACTER', 'CHECK', 'COLLATE', 'COLUMN', \
-					'CONDITION', 'CONSTRAINT', 'CONTINUE', 'CONVERT', 'CREATE', 'CROSS', 'CURRENT_DATE', \
-					'CURRENT_TIME', 'CURRENT_TIMESTAMP', 'CURRENT_USER', 'CURSOR', 'DATABASE', 'DATABASES', \
-					'DAY_HOUR', 'DAY_MICROSECOND', 'DAY_MINUTE', 'DAY_SECOND', 'DEC', 'DECIMAL', 'DECLARE', \
-					'DEFAULT', 'DELAYED', 'DELAY_KEY_WRITE', 'DELETE', 'DESC', 'DESCRIBE', 'DETERMINISTIC', \
-					'DISTINCT', 'DISTINCTROW', 'DIV', 'DOUBLE', 'DROP', 'DUAL', 'EACH', 'ELSE', 'ELSEIF', \
-					'ENCLOSED', 'ESCAPED', 'EXISTS', 'EXIT', 'EXPLAIN', 'FALSE', 'FETCH', 'FLOAT', 'FLOAT4', \
-					'FLOAT8', 'FOR', 'FORCE', 'FOREIGN', 'FROM', 'FULLTEXT', 'GENERATED',  'GET', 'GRANT', \
-					'GROUP', 'HAVING', 'HIGH_PRIORITY', 'HOUR_MICROSECOND', 'HOUR_MINUTE', 'HOUR_SECOND', \
-					'IF', 'IGNORE', 'IN', 'INDEX', 'INFILE', 'INITIAL_SIZE', 'INNER', 'INOUT', 'INSENSITIVE', \
-					'INSERT', 'INSERT_METHOD', 'INT', 'INT1', 'INT2', 'INT3', 'INT4', 'INT8', 'INTEGER', \
-					'INTERVAL', 'INTO', 'IO_AFTER_GTIDS', 'IO_BEFORE_GTIDS', 'IS', 'ITERATE', 'JOIN', 'KEY', \
-					'KEYS', 'KEY_BLOCK_SIZE', 'KILL', 'LEADING', 'LEAVE', 'LEAVES', 'LEFT', 'LIKE', 'LIMIT', \
-					'LINEAR', 'LINES', 'LOAD', 'LOCALTIME', 'LOCALTIMESTAMP', 'LOCK', 'LONG', 'LONGBLOB', \
-					'LONGTEXT', 'LOOP', 'LOW_PRIORITY', 'MASTER_BIND', 'MASTER_SSL_VERIFY_SERVER_CERT', \
-					'MATCH', 'MAXVALUE', 'MEDIUMBLOB', 'MEDIUMINT', 'MEDIUMTEXT', 'MIDDLEINT', \
-					'MINUTE_MICROSECOND', 'MINUTE_SECOND', 'MOD', 'MODIFIES', 'NATURAL', 'NOT', \
-					'NO_WRITE_TO_BINLOG', 'NULL', 'NUMERIC', 'ON', 'OPTIMIZE', 'OPTIMIZER_COSTS', 'OPTION', \
-					'OPTIONALLY', 'OR', 'ORDER', 'OUT', 'OUTER', 'OUTFILE', 'PARTITION', 'PRECISION', \
-					'PRIMARY', 'PROCEDURE', 'PURGE', 'RANGE', 'READ', 'READS', 'READ_WRITE', 'REAL', \
-					'REFERENCES', 'REGEXP', 'RELEASE', 'RENAME', 'REPEAT', 'REPEATABLE', 'REPLACE', 'REQUIRE', \
-					'RESIGNAL', 'RESTRICT', 'RETURN', 'REVOKE', 'RIGHT', 'RLIKE', 'SCHEMA', 'SCHEMAS', \
-					'SECOND_MICROSECOND', 'SELECT', 'SENSITIVE', 'SEPARATOR', 'SET', 'SHOW', 'SIGNAL', \
-					'SMALLINT', 'SPATIAL', 'SPECIFIC', 'SQL', 'SQLEXCEPTION', 'SQLSTATE', 'SQLWARNING', \
-					'SQL_BIG_RESULT', 'SQL_CALC_FOUND_ROWS', 'SQL_SMALL_RESULT', 'SSL', 'STARTING', 'STORED', \
-					'STRAIGHT_JOIN', 'TABLE', 'TERMINATED', 'THEN', 'TINYBLOB', 'TINYINT', 'TINYTEXT', 'TO', \
-					'TRAILING', 'TRIGGER', 'TRUE', 'UNDO', 'UNION', 'UNIQUE', 'UNLOCK', 'UNSIGNED', 'UPDATE', \
-					'USAGE', 'USE', 'USING', 'UTC_DATE', 'UTC_TIME', 'UTC_TIMESTAMP', 'VALUES', 'VARBINARY', \
-					'VARCHAR', 'VARCHARACTER', 'VARYING', 'VIRTUAL',  'WHEN', 'WHERE', 'WHILE', 'WITH', \
-					'WRITE', 'XOR', 'YEAR_MONTH', 'ZEROFILL' ]
+db_reserved = [ 'ACCESSIBLE', 'ADD', 'ALL', 'ALTER', 'ANALYZE', 'AND', 'AS', 'ASC', 'ASENSITIVE', \
+				'BEFORE', 'BETWEEN', 'BIGINT', 'BINARY', 'BLOB', 'BOTH', 'BY', 'CALL', 'CASCADE', \
+				'CASE', 'CATALOG_NAME', 'CHANGE', 'CHAR', 'CHARACTER', 'CHECK', 'COLLATE', 'COLUMN', \
+				'CONDITION', 'CONSTRAINT', 'CONTINUE', 'CONVERT', 'CREATE', 'CROSS', 'CURRENT_DATE', \
+				'CURRENT_TIME', 'CURRENT_TIMESTAMP', 'CURRENT_USER', 'CURSOR', 'DATABASE', 'DATABASES', \
+				'DAY_HOUR', 'DAY_MICROSECOND', 'DAY_MINUTE', 'DAY_SECOND', 'DEC', 'DECIMAL', 'DECLARE', \
+				'DEFAULT', 'DELAYED', 'DELAY_KEY_WRITE', 'DELETE', 'DESC', 'DESCRIBE', 'DETERMINISTIC', \
+				'DISTINCT', 'DISTINCTROW', 'DIV', 'DOUBLE', 'DROP', 'DUAL', 'EACH', 'ELSE', 'ELSEIF', \
+				'ENCLOSED', 'ESCAPED', 'EXISTS', 'EXIT', 'EXPLAIN', 'FALSE', 'FETCH', 'FLOAT', 'FLOAT4', \
+				'FLOAT8', 'FOR', 'FORCE', 'FOREIGN', 'FROM', 'FULLTEXT', 'GENERATED',  'GET', 'GRANT', \
+				'GROUP', 'HAVING', 'HIGH_PRIORITY', 'HOUR_MICROSECOND', 'HOUR_MINUTE', 'HOUR_SECOND', \
+				'IF', 'IGNORE', 'IN', 'INDEX', 'INFILE', 'INITIAL_SIZE', 'INNER', 'INOUT', 'INSENSITIVE', \
+				'INSERT', 'INSERT_METHOD', 'INT', 'INT1', 'INT2', 'INT3', 'INT4', 'INT8', 'INTEGER', \
+				'INTERVAL', 'INTO', 'IO_AFTER_GTIDS', 'IO_BEFORE_GTIDS', 'IS', 'ITERATE', 'JOIN', 'KEY', \
+				'KEYS', 'KEY_BLOCK_SIZE', 'KILL', 'LEADING', 'LEAVE', 'LEAVES', 'LEFT', 'LIKE', 'LIMIT', \
+				'LINEAR', 'LINES', 'LOAD', 'LOCALTIME', 'LOCALTIMESTAMP', 'LOCK', 'LONG', 'LONGBLOB', \
+				'LONGTEXT', 'LOOP', 'LOW_PRIORITY', 'MASTER_BIND', 'MASTER_SSL_VERIFY_SERVER_CERT', \
+				'MATCH', 'MAXVALUE', 'MEDIUMBLOB', 'MEDIUMINT', 'MEDIUMTEXT', 'MIDDLEINT', \
+				'MINUTE_MICROSECOND', 'MINUTE_SECOND', 'MOD', 'MODIFIES', 'NATURAL', 'NOT', \
+				'NO_WRITE_TO_BINLOG', 'NULL', 'NUMERIC', 'ON', 'OPTIMIZE', 'OPTIMIZER_COSTS', 'OPTION', \
+				'OPTIONALLY', 'OR', 'ORDER', 'OUT', 'OUTER', 'OUTFILE', 'PARTITION', 'PRECISION', \
+				'PRIMARY', 'PROCEDURE', 'PURGE', 'RANGE', 'READ', 'READS', 'READ_WRITE', 'REAL', \
+				'REFERENCES', 'REGEXP', 'RELEASE', 'RENAME', 'REPEAT', 'REPEATABLE', 'REPLACE', 'REQUIRE', \
+				'RESIGNAL', 'RESTRICT', 'RETURN', 'REVOKE', 'RIGHT', 'RLIKE', 'SCHEMA', 'SCHEMAS', \
+				'SECOND_MICROSECOND', 'SELECT', 'SENSITIVE', 'SEPARATOR', 'SET', 'SHOW', 'SIGNAL', \
+				'SMALLINT', 'SPATIAL', 'SPECIFIC', 'SQL', 'SQLEXCEPTION', 'SQLSTATE', 'SQLWARNING', \
+				'SQL_BIG_RESULT', 'SQL_CALC_FOUND_ROWS', 'SQL_SMALL_RESULT', 'SSL', 'STARTING', 'STORED', \
+				'STRAIGHT_JOIN', 'TABLE', 'TERMINATED', 'THEN', 'TINYBLOB', 'TINYINT', 'TINYTEXT', 'TO', \
+				'TRAILING', 'TRIGGER', 'TRUE', 'UNDO', 'UNION', 'UNIQUE', 'UNLOCK', 'UNSIGNED', 'UPDATE', \
+				'USAGE', 'USE', 'USING', 'UTC_DATE', 'UTC_TIME', 'UTC_TIMESTAMP', 'VALUES', 'VARBINARY', \
+				'VARCHAR', 'VARCHARACTER', 'VARYING', 'VIRTUAL',  'WHEN', 'WHERE', 'WHILE', 'WITH', \
+				'WRITE', 'XOR', 'YEAR_MONTH', 'ZEROFILL' ]
 
 # =========
 # Functions
 # =========
 
-def CapitalizeSubsystem( subsystem ):
-	"""Certain CSCs used to be capitalized in non-standard ways. This function is holdover and should be removed."""
-	return subsystem
-
-def GetSubsystemVersion( string ):
-	# Right now, the topic version is controlled manually, which requries a 
-	# manual configuration file.  If/when this is formallized, switch to
-	# that source.
-	"""Get the version of the CSC topic (command, event or telemetry)."""
-	version = ""
-	with open(os.environ['HOME'] + "/bin/XML_Versions.txt") as versionfile:
-		for line in versionfile:
-			if string in line:
-				line = versionfile.next()
-				while line != "\n":
-					version+=line.split(" ")[1].rstrip() + "\\n"
-					line = versionfile.next()   #.split(" ")[1].rstrip()
-	return version[:-2]
-
+def get_xmlfile_csc_topic():
+	pkgroot = pathlib.Path(__file__).resolve().parents[1]
+	arguments = []
+	for csc in subsystems:
+		xml_path = pkgroot / "sal_interfaces" / csc
+		for xmlfile in xml_path.glob(f"{csc}_*.xml"):
+			topic = xmlfile.stem.split("_")[1]
+			arguments.append((xmlfile,csc,topic))
+	return arguments
 
