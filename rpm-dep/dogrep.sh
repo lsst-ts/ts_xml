98322b2557e8c7ad16fbd8631e4d4f50fe84ed59
Dave Mills <dmills@lsst.org>
2020-01-31 13:36:44 -0700

    Add rpm-dep scripts

---
+++ rpm-dep/dogrep.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+find $1 -type f -not -path "./ts_xml/*" -not -path "./grepper/*" -name '*.py'        -exec grep -l SALPY_$2 {} \; > refs/$2
+find $1 -type f -not -path "./ts_xml/*" -not -path "./grepper/*" -name '*.ipynb'     -exec grep -l SALPY_$2 {} \; >> refs/$2
+find $1 -type f -not -path "./ts_xml/*" -not -path "./grepper/*" -name '*.vi'        -exec grep -l SALLV_$2 {} \; >> refs/$2
+find $1 -type f -not -path "./ts_xml/*" -not -path "./grepper/*" -name '*.cpp'       -exec grep -l "SAL_$2.h" {} \; >> refs/$2
+find $1 -type f -not -path "./ts_xml/*" -not -path "./grepper/*" -name '*.h'         -exec grep -l "SAL_$2.h" {} \; >> refs/$2
+find $1 -type f -not -path "./ts_xml/*" -not -path "./grepper/*" -name '*.java'      -exec grep -l "org.lsst.sal.SAL_$2" {} \; >> refs/$2
+find $1 -type f -not -path "./ts_xml/*" -not -path "./grepper/*" -name '*.sal'       -exec grep -l "org.lsst.sal.SAL_$2" {} \; >> refs/$2
+find $1 -type f -not -path "./ts_xml/*" -not -path "./grepper/*" -name '*.yaml'      -exec grep -l "device: $2" {} \; >> refs/$2
+find $1 -type f -not -path "./ts_xml/*" -not -path "./grepper/*" -name 'Jenkinsfile' -exec grep -l " $2 " {} \; >> refs/$2
+find $1 -type f -not -path "./ts_xml/*" -not -path "./grepper/*" -name '*.js'        -exec grep -l "name: '$2'" {} \; >> refs/$2
+echo "find $1 -type f -name '*.py'       -exec grep -l 'salobj.Remote.*$2' {} \; >> refs/$2" > /tmp/doit
+echo "find $1 -type f -name '*.py'       -exec grep -l 'salobj.Controller.*$2' {} \; >> refs/$2" >> /tmp/doit
+echo "find $1 -type f -name '*.jsx'      -exec grep -l '\\-$2\\-' {} \; >> refs/$2" >> /tmp/doit
+echo "find $1 -type f -name 'Dockerfile' -exec grep -l '$2\\-' {} \; >> refs/$2" >> /tmp/doit
+chmod 755 /tmp/doit
+/tmp/doit
+
---
+++ rpm-dep/grepcsc.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo "Prescan tidyup"
+rm -fr refs
+mkdir refs
+
+git clone https://github.com/lsst-ts/ts_xml.git
+grep "<Name>" ts_xml/sal_interfaces/SALSubsystems.xml > csclist
+perl -pi -w -e 's/  <Name>//g;' csclist
+perl -pi -w -e 's/<\/Name>//g;' csclist
+cscs=`cat csclist`
+
+for csc in $cscs
+do
+   echo "Grepping for $csc"
+   ./dogrep.sh $1 $csc
+done
+
+cd refs
+echo "Building deps for $1"
+grep -l $1 * > $1.deps
+echo "Dependencies for $1 are"
+cat $1.deps
+
+
