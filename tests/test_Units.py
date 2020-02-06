56e3034bd836b47f3afdf76fa6824723dc66d5ab
Dave Mills <dmills@lsst.org>
2020-02-06 09:38:25 -0700

    Fix MTM1M3 Units and Descriptions, and rename from m1m3 baseline

--- tests/test_Units.py
+++ tests/test_Units.py
@@ -13,8 +13,6 @@ def check_for_issues(csc, topic):
         jira = "CAP-318"
     elif csc == "MTEEC" and topic == "Commands":
         jira = "DM-22159"
-    elif csc == "MTM1M3":
-        jira = "DM-20956"
     else:
         jira = ""
     return jira
