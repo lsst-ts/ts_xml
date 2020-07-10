da11de539a4d7cc25c00ead31fbf06d3310fdd86
Rob Bovill <rbovill@lsst.org>
2020-07-10 11:39:43 -0700

    Starting to add the unit test to verify strings are dimensionless.

--- tests/test_Units.py
+++ tests/test_Units.py
@@ -50,3 +50,31 @@ def test_units(xmlfile, csc, topic):
             assert type(ts_xml.check_unit(unit.text)) is astropy.units.Quantity
             "Unit '" + unit.text + "' in " + str(xmlfile.name) + \
                 " does not meet astropy standards."
+
+@pytest.mark.parametrize("xmlfile,csc,topic", ts_xml.get_xmlfile_csc_topic())
+def test_string_units(xmlfile, csc, topic):
+    """Test that the <Units> field for STRING-type attributes is 'unitless.'
+
+    Parameters
+    ----------
+    xmlfile : `pathlib.Path`
+        Full filepath to the Commands or Events XML file for the CSC.
+    csc : `testutils.subsystems`
+        Name of the CSC
+    topic : `xmlfile.stem`
+        One of ['Commands','Events','Telemetry']
+    """
+    saltype = "SAL" + topic.rstrip('s')
+    # Check for known issues.
+    jira = check_for_issues(csc, topic)
+    if jira:
+        pytest.skip(jira + ": " + str(xmlfile.name) + " <Unit> fields do not conform to astropy standards.")
+    # Test the attribute <Units> fields.
+    with open(str(xmlfile), "r", encoding="utf-8") as f:
+        tree = et.parse(f)
+    root = tree.getroot()
+    for attrib in root.findall(f"./{saltype}/item"):
+        idltype = attrib.find("IDL_Type")
+        if idltype.text == "string":
+            unit = attrib.find("Units")
+            assert unit.text in ("unitless", "dimensionless")
